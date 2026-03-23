e04f52654d52522141de992425ee34b412866f84
vapor <18055623414@163.com>
2026-03-23 12:38:58 +0800

    refactor: 修改 campus-review-service/admin-service/src/main/java/com/vapor/admin/service/AdminOrchestratorService.java

--- campus-review-service/admin-service/src/main/java/com/vapor/admin/service/AdminOrchestratorService.java
+++ campus-review-service/admin-service/src/main/java/com/vapor/admin/service/AdminOrchestratorService.java
@@ -4,7 +4,9 @@ import com.vapor.common.api.ApiResponse;
 import com.vapor.common.error.BizException;
 import com.vapor.common.util.UserContextUtil;
 import com.vapor.common.web.UserContext;
+import com.vapor.model.restaurant.RestaurantDTO;
 import com.vapor.model.review.ReviewDTO;
+import com.vapor.model.user.UserDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -24,10 +26,15 @@ public class AdminOrchestratorService {
     private static final Logger log = LoggerFactory.getLogger(AdminOrchestratorService.class);
     private static final ParameterizedTypeReference<ApiResponse<List<ReviewDTO>>> REVIEW_LIST_TYPE =
             new ParameterizedTypeReference<>() {};
+    private static final ParameterizedTypeReference<ApiResponse<List<UserDTO>>> USER_LIST_TYPE =
+            new ParameterizedTypeReference<>() {};
+    private static final ParameterizedTypeReference<ApiResponse<List<RestaurantDTO>>> RESTAURANT_LIST_TYPE =
+            new ParameterizedTypeReference<>() {};
 
     private final RestClient restClient;
     private final String userServiceBaseUrl;
     private final String reviewServiceBaseUrl;
+    private final String restaurantServiceBaseUrl;
 
     /**
      * 构造编排服务。
@@ -35,15 +42,18 @@ public class AdminOrchestratorService {
      * @param restClient HTTP 客户端
      * @param userServiceBaseUrl 用户服务基础地址
      * @param reviewServiceBaseUrl 评价服务基础地址
+     * @param restaurantServiceBaseUrl 餐厅服务基础地址
      */
     public AdminOrchestratorService(
             RestClient restClient,
             @Value("${downstream.user-service-base-url}") String userServiceBaseUrl,
-            @Value("${downstream.review-service-base-url}") String reviewServiceBaseUrl
+            @Value("${downstream.review-service-base-url}") String reviewServiceBaseUrl,
+            @Value("${downstream.restaurant-service-base-url}") String restaurantServiceBaseUrl
     ) {
         this.restClient = restClient;
         this.userServiceBaseUrl = userServiceBaseUrl;
         this.reviewServiceBaseUrl = reviewServiceBaseUrl;
+        this.restaurantServiceBaseUrl = restaurantServiceBaseUrl;
     }
 
     /**
@@ -102,6 +112,25 @@ public class AdminOrchestratorService {
     }
 
     /**
+     * 获取用户列表。
+     *
+     * @return 用户列表
+     * @throws BizException 非管理员时抛出
+     */
+    public List<UserDTO> getUserList() {
+        UserContext ctx = UserContextUtil.requireUserContext();
+        log.info("获取用户列表：adminId={}", ctx.getUserId());
+
+        ApiResponse<List<UserDTO>> resp = restClient.get()
+                .uri(userServiceBaseUrl + "/api/admin/users")
+                .header("X-User-Id", String.valueOf(ctx.getUserId()))
+                .header("X-User-Roles", String.join(",", ctx.getRoles()))
+                .retrieve()
+                .body(USER_LIST_TYPE);
+        return resp == null || resp.getData() == null ? List.of() : resp.getData();
+    }
+
+    /**
      * 封禁指定用户。
      *
      * @param userId 用户 ID
@@ -131,6 +160,43 @@ public class AdminOrchestratorService {
 
         restClient.post()
                 .uri(userServiceBaseUrl + "/api/admin/users/" + userId + "/unban")
+                .header("X-User-Id", String.valueOf(ctx.getUserId()))
+                .header("X-User-Roles", String.join(",", ctx.getRoles()))
+                .retrieve()
+                .toBodilessEntity();
+    }
+
+    /**
+     * 获取餐厅列表。
+     *
+     * @return 餐厅列表
+     * @throws BizException 非管理员时抛出
+     */
+    public List<RestaurantDTO> getRestaurantList() {
+        UserContext ctx = UserContextUtil.requireUserContext();
+        log.info("获取餐厅列表：adminId={}", ctx.getUserId());
+
+        ApiResponse<List<RestaurantDTO>> resp = restClient.get()
+                .uri(restaurantServiceBaseUrl + "/api/admin/restaurants")
+                .header("X-User-Id", String.valueOf(ctx.getUserId()))
+                .header("X-User-Roles", String.join(",", ctx.getRoles()))
+                .retrieve()
+                .body(RESTAURANT_LIST_TYPE);
+        return resp == null || resp.getData() == null ? List.of() : resp.getData();
+    }
+
+    /**
+     * 删除指定餐厅。
+     *
+     * @param restaurantId 餐厅 ID
+     * @throws BizException 非管理员时抛出
+     */
+    public void deleteRestaurant(Long restaurantId) {
+        UserContext ctx = UserContextUtil.requireUserContext();
+        log.info("删除餐厅：restaurantId={}, adminId={}", restaurantId, ctx.getUserId());
+
+        restClient.delete()
+                .uri(restaurantServiceBaseUrl + "/api/admin/restaurants/" + restaurantId)
                 .header("X-User-Id", String.valueOf(ctx.getUserId()))
                 .header("X-User-Roles", String.join(",", ctx.getRoles()))
                 .retrieve()
