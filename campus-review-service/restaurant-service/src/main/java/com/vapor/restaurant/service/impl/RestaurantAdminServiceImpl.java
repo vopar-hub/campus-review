aa5d6c6a2f00fe4304ccc576f3d396b66e88c581
vapor <18055623414@163.com>
2026-03-24 12:23:04 +0800

    refactor: 修改 campus-review-service/restaurant-service/src/main/java/com/vapor/restaurant/service/impl/RestaurantAdminServiceImpl.java

--- campus-review-service/restaurant-service/src/main/java/com/vapor/restaurant/service/impl/RestaurantAdminServiceImpl.java
+++ campus-review-service/restaurant-service/src/main/java/com/vapor/restaurant/service/impl/RestaurantAdminServiceImpl.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.vapor.common.error.BizException;
 import com.vapor.common.error.ErrorCode;
 import com.vapor.common.util.UserContextUtil;
+import com.vapor.model.restaurant.RestaurantCreateRequest;
 import com.vapor.model.restaurant.RestaurantDTO;
 import com.vapor.restaurant.entity.RestaurantEntity;
 import com.vapor.restaurant.mapper.RestaurantMapper;
@@ -13,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
 import java.util.List;
 
 /**
