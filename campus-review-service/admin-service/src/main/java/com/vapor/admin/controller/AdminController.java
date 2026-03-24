07b2f1fcd4fe66510e7a1cce08e25c81eba26f88
vapor <18055623414@163.com>
2026-03-24 11:51:47 +0800

    refactor: 修改 campus-review-service/admin-service/src/main/java/com/vapor/admin/controller/AdminController.java

--- campus-review-service/admin-service/src/main/java/com/vapor/admin/controller/AdminController.java
+++ campus-review-service/admin-service/src/main/java/com/vapor/admin/controller/AdminController.java
@@ -2,14 +2,17 @@ package com.vapor.admin.controller;
 
 import com.vapor.admin.service.AdminOrchestratorService;
 import com.vapor.common.api.ApiResponse;
+import com.vapor.model.restaurant.RestaurantCreateRequest;
 import com.vapor.model.restaurant.RestaurantDTO;
 import com.vapor.model.user.UserDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -81,6 +84,18 @@ public class AdminController {
     @Operation(summary = "获取餐厅列表", description = "获取所有餐厅列表")
     public ApiResponse<List<RestaurantDTO>> getRestaurantList() {
         return ApiResponse.ok(adminOrchestratorService.getRestaurantList());
+    }
+
+    /**
+     * 创建餐厅。
+     *
+     * @param request 创建请求
+     * @return 创建的餐厅 DTO
+     */
+    @PostMapping("/restaurants")
+    @Operation(summary = "创建餐厅", description = "添加新的餐厅信息")
+    public ApiResponse<RestaurantDTO> createRestaurant(@Valid @RequestBody RestaurantCreateRequest request) {
+        return ApiResponse.ok(adminOrchestratorService.createRestaurant(request));
     }
 
     /**
