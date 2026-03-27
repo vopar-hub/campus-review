04da568854510aa5c1c57f104410117dded272da
vapor <18055623414@163.com>
2026-03-27 20:39:59 +0800

    refactor: 修改 campus-review-service/ranking-service/src/main/java/com/vapor/ranking/client/InteractionServiceClient.java

--- campus-review-service/ranking-service/src/main/java/com/vapor/ranking/client/InteractionServiceClient.java
+++ campus-review-service/ranking-service/src/main/java/com/vapor/ranking/client/InteractionServiceClient.java
@@ -2,6 +2,7 @@ package com.vapor.ranking.client;
 
 import com.vapor.common.api.ApiResponse;
 import com.vapor.model.interaction.InteractionCountDTO;
+import com.vapor.ranking.client.fallback.InteractionServiceClientFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
