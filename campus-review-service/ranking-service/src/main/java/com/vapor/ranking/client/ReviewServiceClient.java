13f3ef8910a96da9478a1b6adaad9aa733fd0fa3
vapor <18055623414@163.com>
2026-03-27 20:40:04 +0800

    refactor: 修改 campus-review-service/ranking-service/src/main/java/com/vapor/ranking/client/ReviewServiceClient.java

--- campus-review-service/ranking-service/src/main/java/com/vapor/ranking/client/ReviewServiceClient.java
+++ campus-review-service/ranking-service/src/main/java/com/vapor/ranking/client/ReviewServiceClient.java
@@ -2,6 +2,7 @@ package com.vapor.ranking.client;
 
 import com.vapor.common.api.ApiResponse;
 import com.vapor.model.review.ReviewDTO;
+import com.vapor.ranking.client.fallback.ReviewServiceClientFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
