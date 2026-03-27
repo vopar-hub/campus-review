a181678493e1094ae30406d1f5812a139919f1d9
vapor <18055623414@163.com>
2026-03-27 20:33:56 +0800

    refactor: 修改 campus-review-service/admin-service/src/main/java/com/vapor/admin/config/FeignRequestInterceptor.java

--- campus-review-service/admin-service/src/main/java/com/vapor/admin/config/FeignRequestInterceptor.java
+++ campus-review-service/admin-service/src/main/java/com/vapor/admin/config/FeignRequestInterceptor.java
@@ -4,6 +4,8 @@ import com.vapor.common.web.UserContext;
 import com.vapor.common.web.UserContextHolder;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 /**
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class FeignRequestInterceptor implements RequestInterceptor {
+    private static final Logger log = LoggerFactory.getLogger(FeignRequestInterceptor.class);
 
     @Override
     public void apply(RequestTemplate template) {
@@ -18,6 +21,11 @@ public class FeignRequestInterceptor implements RequestInterceptor {
         if (ctx != null && ctx.getUserId() != null) {
             template.header("X-User-Id", String.valueOf(ctx.getUserId()));
             template.header("X-User-Roles", String.join(",", ctx.getRoles()));
+            log.debug("Feign 请求拦截器已添加用户上下文：userId={}, roles={}, request={}.{}",
+                      ctx.getUserId(), ctx.getRoles(), template.method(), template.url());
+        } else {
+            log.debug("Feign 请求拦截器 - 未登录用户，跳过上下文传递：{}.{}",
+                      template.method(), template.url());
         }
     }
 }
