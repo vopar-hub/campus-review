25002270378832a6d106df5c95b704913668f43a
vapor <18055623414@163.com>
2026-03-28 16:43:40 +0800

    refactor: 修改 campus-review-service/review-service/src/main/java/com/vapor/review/interaction/mapper/LikeMapper.java

---
+++ campus-review-service/review-service/src/main/java/com/vapor/review/interaction/mapper/LikeMapper.java
@@ -0,0 +1,12 @@
+package com.vapor.review.interaction.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.vapor.review.interaction.entity.LikeEntity;
+
+/**
+ * 点赞表数据访问接口。
+ *
+ * 基于 MyBatis-Plus {@link BaseMapper} 提供 CRUD 能力。
+ */
+public interface LikeMapper extends BaseMapper<LikeEntity> {
+}
