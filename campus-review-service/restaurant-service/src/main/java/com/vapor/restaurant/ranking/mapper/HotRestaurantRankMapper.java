e5d4617cc1f9609b8aba0610a2a70a72f4f62808
vapor <18055623414@163.com>
2026-03-28 16:47:09 +0800

    refactor: 修改 campus-review-service/restaurant-service/src/main/java/com/vapor/restaurant/ranking/mapper/HotRestaurantRankMapper.java

---
+++ campus-review-service/restaurant-service/src/main/java/com/vapor/restaurant/ranking/mapper/HotRestaurantRankMapper.java
@@ -0,0 +1,27 @@
+package com.vapor.restaurant.ranking.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.vapor.restaurant.ranking.entity.HotRestaurantRankEntity;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/**
+ * 热门餐馆榜数据访问接口。
+ *
+ * 基于 MyBatis-Plus {@link BaseMapper} 提供 CRUD 能力。
+ */
+public interface HotRestaurantRankMapper extends BaseMapper<HotRestaurantRankEntity> {
+
+    /**
+     * 根据餐馆 ID 列表查询排行榜数据。
+     *
+     * @param restaurantIds 餐馆 ID 列表
+     * @return 排行榜实体列表
+     */
+    @Select("<script>SELECT * FROM hot_restaurant_rank WHERE restaurant_id IN " +
+            "<foreach collection='ids' item='id' open='(' separator=',' close=')'>#{id}</foreach>" +
+            "</script>")
+    List<HotRestaurantRankEntity> selectByRestaurantIds(@Param("ids") List<Long> restaurantIds);
+}
