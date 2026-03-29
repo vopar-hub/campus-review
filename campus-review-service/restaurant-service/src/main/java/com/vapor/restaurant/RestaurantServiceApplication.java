592b7099ed7c40724b1015f0ffc10fe86d6a0b7f
vapor <18055623414@163.com>
2026-03-29 17:18:03 +0800

    refactor: 修改 campus-review-service/restaurant-service/src/main/java/com/vapor/restaurant/RestaurantServiceApplication.java

--- campus-review-service/restaurant-service/src/main/java/com/vapor/restaurant/RestaurantServiceApplication.java
+++ campus-review-service/restaurant-service/src/main/java/com/vapor/restaurant/RestaurantServiceApplication.java
@@ -4,6 +4,7 @@ import com.vapor.restaurant.service.MinioService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -13,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * 提供餐馆创建、查询与检索等能力。
  */
 @SpringBootApplication(scanBasePackages = "com.vapor")
+@EnableFeignClients
 @EnableScheduling
 public class RestaurantServiceApplication {
     /**
