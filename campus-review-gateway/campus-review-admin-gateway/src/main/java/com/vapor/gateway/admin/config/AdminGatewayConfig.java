effad5872a0390bebb1a132c7d7e4e5eabc57283
vapor <18055623414@163.com>
2026-03-29 12:12:53 +0800

    refactor: 修改 campus-review-gateway/campus-review-admin-gateway/src/main/java/com/vapor/gateway/admin/config/AdminGatewayConfig.java

--- campus-review-gateway/campus-review-admin-gateway/src/main/java/com/vapor/gateway/admin/config/AdminGatewayConfig.java
+++ campus-review-gateway/campus-review-admin-gateway/src/main/java/com/vapor/gateway/admin/config/AdminGatewayConfig.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vapor.gateway.admin.filter.AdminJwtAuthGlobalFilter;
 import com.vapor.gateway.admin.filter.RequestIdGlobalFilter;
 import com.vapor.gateway.admin.filter.SecurityHeadersGlobalFilter;
-import com.vapor.utils.jwt.JwtService;
+import com.vapor.common.util.jwt.JwtService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
