a1f5435379ea021e60dd3614e65ab3d225463824
vapor <18055623414@163.com>
2026-03-29 14:02:19 +0800

    refactor: 修改 campus-review-model/src/main/java/com/vapor/model/user/UserDTO.java

--- campus-review-model/src/main/java/com/vapor/model/user/UserDTO.java
+++ campus-review-model/src/main/java/com/vapor/model/user/UserDTO.java
@@ -10,6 +10,7 @@ import java.util.Set;
  * @param email 邮箱
  * @param studentNo 学号
  * @param nickname 昵称
+ * @param avatarUrl 头像URL
  * @param roles 角色集合
  * @param banned 是否封禁
  * @param createdAt 创建时间
@@ -19,6 +20,7 @@ public record UserDTO(
         String email,
         String studentNo,
         String nickname,
+        String avatarUrl,
         Set<String> roles,
         boolean banned,
         Instant createdAt
